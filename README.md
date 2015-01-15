c4c1d26337549b95ab5b5696e171f0495c32955e
falkTX <falktx@gmail.com>
2015-01-15 21:48:28 +0000

    Initial UI file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mod-panel
+
+mod-panel is a simple control panel to start MOD UI and SDK services.
---
+++ mod-panel.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# MOD-Panel
+# Copyright (C) 2014-2015 Filipe Coelho <falktx@falktx.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of
+# the License, or any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# For a full copy of the GNU General Public License see the LICENSE file.
+
+### NOTE: Due to mod-sdk not being python3 ready we have to use python2 for now
