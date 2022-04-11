4ca3bc5f473392533cd7bebe73ba68b8e630af50
falkTX <falktx@falktx.com>
2022-04-11 20:22:02 +0100

    print messages when tools close too fast
    Signed-off-by: falkTX <falktx@falktx.com>

--- source/mod-sdk.sh
+++ source/mod-sdk.sh
@@ -13,6 +13,12 @@ if [ -n "${OLD_PATH}" ]; then
     export PATH="${OLD_PATH}"
 fi
 
+# check for pip3 tool
+if ! which pip3 1>/dev/null 2>/dev/null; then
+    echo "pip3 tool not available, cannot continue"
+    exit 1
+fi
+
 # install virtualenv as needed
 if ! virtualenv --version 2>/dev/null; then
     pip3 install virtualenv
